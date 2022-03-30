f377daf847c33d61433c4e3bac700bfa8218ec8d
HyeonWooGa <yeonwoopark22@gmail.com>
2022-03-31 07:16:59 +0900

    #4.6 Loading Username

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const userName = loginInput.value;
-    localStorage.setItem("userName", userName);
+    localStorage.setItem(USERNAME_KEY, userName);
     //greeting.innerText = "Hello " + userName;
+    paintGreetings(userName);
+}
+
+function paintGreetings(userName) {
     greeting.innerText = `Hello ${userName} keep going`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+const savedUserName = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener("submit", onLoginSubmit);
+if (savedUserName === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUserName);
+}
