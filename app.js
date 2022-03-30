aa90732281f3a9be2d2cbbb94688c3cf7fa76365
HyeonWooGa <yeonwoopark22@gmail.com>
2022-03-31 06:04:21 +0900

    #4.3 Events part Two

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault();
     const userName = loginInput.value;
@@ -8,4 +10,12 @@ function onLoginSubmit(event) {
     console.log(event);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit)
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.log(event);
+    console.dir(event);
+    alert("clicked!");
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
