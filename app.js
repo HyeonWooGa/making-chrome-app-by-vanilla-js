8ab532b4c8a839b2c09175ac8564e0bb3884438d
HyeonWooGa <yeonwoopark22@gmail.com>
2022-03-31 04:23:56 +0900

    #4.0 Input Values
    I start to make momentum app by using JS, HTML, CSS

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const h1 = document.querySelector(".hello h1");
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
 
-
-function handleTitleClick() {
-    h1.classList.toggle("clicked")
+function onLoginBtnClick() {
+    console.log("Hello", loginInput.value);
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", onLoginBtnClick)
